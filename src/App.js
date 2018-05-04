8d283023a6bd128c6af1b5727f0c71e4e38ff2d2
PK Banks <pkbanks12@gmail.com>
2018-05-04 18:03:31 -0400

    axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IdeasContainer from './components/IdeasContainer';
 
 class App extends Component {
   render() {
