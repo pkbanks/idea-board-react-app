f9dee91f82ef68bb7b2b5b1fe8f7aade1c3f5ef0
PK Banks <pkbanks12@gmail.com>
2018-05-04 18:46:55 -0400

    post

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -21,13 +21,30 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return(
       <div>
         {this.state.ideas.map((idea) => {
           return (<Idea idea={idea} key={idea.id} />)
         })}
-        <button className="newIdeaButton">
+        <button className="newIdeaButton" onClick={this.addNewIdea} >
           New Idea
         </button>
       </div>
