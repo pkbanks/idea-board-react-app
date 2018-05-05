67eb07ff78a194fdd7839d7607789886a69c9260
PK Banks <pkbanks12@gmail.com>
2018-05-05 11:36:34 -0400

    edit/update ideas

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -47,14 +47,22 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {
+      [ideaIndex]: { $set: idea }
+    })
+    this.setState({ideas: ideas})
+  }
+
   render() {
     return(
       <div>
         {this.state.ideas.map((idea) => {
           if(this.state.editingIdeaId === idea.id) {
-            return(<IdeaForm idea={idea} key={idea.id} />)
+            return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} />)
           } else {
-            return (<Idea idea={idea} key={idea.id} />)
+            return (<Idea idea={idea} key={idea.id} updateIdea={this.updateIdea} />)
           }
         })}
         <button className="newIdeaButton" onClick={this.addNewIdea} >
