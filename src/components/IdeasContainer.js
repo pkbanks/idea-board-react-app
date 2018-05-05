70c6309d8e8557dea520c70b3216c6e9c17485d6
PK Banks <pkbanks12@gmail.com>
2018-05-05 13:43:54 -0400

    edit/update existing ideas

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -12,7 +12,8 @@ class IdeasContainer extends Component {
     super(props)
     this.state = {
       ideas: [],
-      editingIdeaId: null
+      editingIdeaId: null,
+      notification: ''
     }
   }
 
@@ -41,7 +42,7 @@ class IdeasContainer extends Component {
       })
       this.setState({
         ideas: ideas,
-        editingIdeaId: response.data.id
+        editingIdeaId: response.data.id,
       })
     })
     .catch(error => console.log(error))
@@ -52,7 +53,20 @@ class IdeasContainer extends Component {
     const ideas = update(this.state.ideas, {
       [ideaIndex]: { $set: idea }
     })
-    this.setState({ideas: ideas})
+    this.setState({
+      ideas: ideas,
+      notification: 'All changes saved'
+    })
+  }
+
+  resetNotification = () => {
+    this.setState({notification: ''})
+  }
+
+  
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id},
+      () => { this.title.focus() })
   }
 
   render() {
@@ -60,14 +74,22 @@ class IdeasContainer extends Component {
       <div>
         {this.state.ideas.map((idea) => {
           if(this.state.editingIdeaId === idea.id) {
-            return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} />)
+            return(<IdeaForm
+              idea={idea}
+              key={idea.id}
+              updateIdea={this.updateIdea}
+              titleRef= {input => this.title = input}
+              resetNotification={this.resetNotification} />)
           } else {
-            return (<Idea idea={idea} key={idea.id} updateIdea={this.updateIdea} />)
+            return (<Idea idea={idea} key={idea.id} onClick={this.enableEditing} />)
           }
         })}
         <button className="newIdeaButton" onClick={this.addNewIdea} >
           New Idea
         </button>
+        <span className="notification">
+          {this.state.notification}
+        </span>
       </div>
     )
 
